fd6a9db63a7ac73bd93d4456e0dd831d8540f0df
MeesterMarcus <lorenzanamarcus@gmail.com>
2022-03-10 13:25:23 -0600

    better message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({a: 1});
+  res.json({success: "The app is running!"});
 });
 
 router.get('/favicon.ico', (req, res) => {

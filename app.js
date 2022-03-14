8b12b1ae46aa01c262bd672deef36d44d24ce0e4
MeesterMarcus <lorenzanamarcus@gmail.com>
2022-03-14 18:06:41 -0500

    establishing mongodb connection, testing queries

--- app.js
+++ app.js
@@ -3,11 +3,47 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require("mongoose");
+require("dotenv").config(); 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+  console.log('connection successful!');
+  // define Schema
+  var BookSchema = mongoose.Schema({
+    name: String,
+    price: Number,
+    quantity: Number
+  });
+
+  // compile schema to model
+  var Book = mongoose.model('Book', BookSchema, 'bookstore');
+
+  // a document instance
+  var book1 = new Book({ name: 'Introduction to Mongoose', price: 10, quantity: 25 });
+
+  // save model to database
+  book1.save(function (err, book) {
+    if (err) return console.error(err);
+    console.log(book.name + " saved to bookstore collection.");
+  });
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

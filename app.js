091bdbbccd8215a32ff421aebbc66554697a6f0f
MeesterMarcus <lorenzanamarcus@gmail.com>
2022-03-15 21:53:00 -0500

    removing old code

--- app.js
+++ app.js
@@ -25,25 +25,7 @@ db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function() {
   console.log('connection successful!');
-  // // define Schema
-  // var BookSchema = mongoose.Schema({
-  //   name: String,
-  //   price: Number,
-  //   quantity: Number
-  // });
-
-  // // compile schema to model
-  // var Book = mongoose.model('Book', BookSchema, 'bookstore');
-
-  // // a document instance
-  // var book1 = new Book({ name: 'Introduction to Mongoose', price: 10, quantity: 25 });
-
-  // // save model to database
-  // book1.save(function (err, book) {
-  //   if (err) return console.error(err);
-  //   console.log(book.name + " saved to bookstore collection.");
-  // });
-})
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
